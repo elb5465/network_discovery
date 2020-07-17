8b46f58f05ffecc7f08daa75b6037ac8dd0399d5
Eric Baker <elb5465@gmail.com>
2020-07-17 08:18:01 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#Taken from https://www.youtube.com/watch?v=nlRZdysaAiI @6:55
+from setuptools import setup
+
+#name is arbitrary: can be anything - just whatever you want to type to call the function from cmd line.
+setup{
+    name='network_discovery',
+    version='0.0.1',
+    url='http://github.com/elb5465/network_discovery',
+    license='MIT',
+    author='Eric Baker',
+    author_email='elb5465@gmail.com',
+    description='Scan the device\'s network that you are connected to and retrieve all connected devices IP/MAC addresses, fqdn, and vendor name, all exported to a JSON file in the directory that this is run in.',
+    py_modules=['network_discovery'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'docopt >= 0.6.0',
+        'mac-vendor-lookup >= 0.1.11'],
+    entry_points="""
+        [console_scripts]
+        network_discovery = cli:main
+        """
+}
